80f1dc75aeb875073bc1d63aadfad925541a0b21
gaojianchen_bjut <gaojianchen_bjut@163.com>
2022-03-22 22:56:31 +0800

    【特性开发】引入ffmpeg库

--- ffmpegDemo/ffmpegDemo/main.cpp
+++ ffmpegDemo/ffmpegDemo/main.cpp
@@ -1,10 +1,22 @@
 #include "ffmpegDemo.h"
 #include <QtWidgets/QApplication>
+#include <stdio.h>
+#include <iostream>
+
+extern "C"
+{
+#include <libavcodec\avcodec.h>
+}
 
 int main(int argc, char *argv[])
 {
-    QApplication a(argc, argv);
-    ffmpegDemo w;
-    w.show();
-    return a.exec();
+    //QApplication a(argc, argv);
+    //ffmpegDemo w;
+    //w.show();
+	printf("test\n");
+	printf("%s", avcodec_configuration());
+	system("pause");
+
+
+	return 0;// a.exec();
 }
